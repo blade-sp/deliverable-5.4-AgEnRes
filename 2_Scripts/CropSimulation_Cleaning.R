32bd8d08d948af4754e5438093da30adec295eb6
Riccardo Spada <riccardo.spada10@gmail.com>
2025-10-22 17:10:59 +0200

    nitrogen-yield relationship

--- 2_Scripts/CropSimulation_Cleaning.R
+++ 2_Scripts/CropSimulation_Cleaning.R
@@ -98,32 +98,32 @@ library(robustbase)
 
 sim_data <- read_csv("1_Data/RawData/EPIC2/Riccardo_EPIC.csv") 
 
-df <- sim_data |> 
+df_sim_data <- sim_data |> 
   filter(CROP == "WWHT", 
-#         SimUID == 52338, # select only one grid location
          IRR == "rf" # rainfed only
-        ) 
+         )
+         
+df_sim_data |> 
+  group_by(WTH) |>
+  summarise(
+    mean_YLD_DM = mean(FTN),
+    sd_YLD_DM = sd(FTN),
+    n = n()
+  ) 
+summary(df_sim_data)
 
-ggplot(df, aes(x = FTN, y = YLD_DM)) +
+# select only one grid location
+df_sim_data <- sim_data |> 
+  filter(SimUID == 52338)
+
+
+
+ggplot(sim_data, aes(x = FTN, y = YLD_DM)) +
   geom_point(alpha = 0.5)
 
 
-
-
-reg_yield_function <- lmrob(YLD_DM ~ sqrt(FTN) + FTN, data = df, method = "MM")
-summary(reg_yield_function)
-
-# append residuals to dataframe
-df <- df %>% 
-  mutate(residuals = reg_yield_function$residuals,
-         abs_residuals = abs(residuals))
-
-reg_variation_function <- lmrob(abs_residuals ~ I(sqrt(FTN)), 
-                                  data = df, method = "MM")
-summary(reg_variation_function)
-
 # plot yield function with data points
-ggplot(df, aes(x = FTN, y = YLD_DM)) +
+ggplot(sim_data, aes(x = FTN, y = YLD_DM)) +
   geom_point(alpha = 0.5) +
   stat_smooth(method = "lm", 
               formula = y ~ sqrt(x) + x, 
